345cc18fbd04ee97ac71aa077c27410b731edf1b
Andreia <andreiacribeiro.5@gmail.com>
2024-06-25 20:09:59 +0100

    added handleCheckedTask functionality

--- to-do-list/src/ToDoList.jsx
+++ to-do-list/src/ToDoList.jsx
@@ -12,7 +12,7 @@ function ToDoList() {
     function addTask() {
         //if condition to prevent add an empty task
         if(newTask.trim() !== "") {
-            setTasks(t => [...t, newTask]);
+            setTasks(t => [...t, { text: newTask, checked: false}]);
 
             setNewTask("");
         }
@@ -41,6 +41,17 @@ function ToDoList() {
         }
     }
 
+    function handleCheckedTask(index) {
+        const updatedTasks = tasks.map((task, i) => 
+            i === index ? { ...task, checked: !task.checked } : task
+        );
+
+        // Move the checked task to the end
+        const [task] = updatedTasks.splice(index, 1);
+        updatedTasks.push(task);
+        setTasks(updatedTasks);
+    }
+
     return(
        <div className="to-do-list-container">
         <h1>To Do List</h1>
@@ -52,10 +63,15 @@ function ToDoList() {
             <ol>
                 {tasks.map((task, index) => 
                     <li key={index}>
-                        <span className="task">{task}</span>
-                        <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
-                        <button className="move-up-button" onClick={() => moveTaskUp(index)}>UP</button>
-                        <button className="move-down-button" onClick={() => moveTaskDown(index)}>DOWN</button>
+                        <span className={`task ${task.checked ? "task-checked" : "task-unchecked"}`}>{task.text}</span>
+                        <img className="image-check" src="./src/assets/vector-image.png" alt="Check image" onClick={() => handleCheckedTask(index)}></img>
+                        {!task.checked && (
+                            <>
+                                <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+                                <button className="move-up-button" onClick={() => moveTaskUp(index)}>UP</button>
+                                <button className="move-down-button" onClick={() => moveTaskDown(index)}>DOWN</button>
+                            </>
+                        )}
                     </li>)}
             </ol>
        </div>
